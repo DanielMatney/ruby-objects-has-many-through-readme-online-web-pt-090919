defd00785f17c73c32d6b01c3205a2cf1e1dc71f
Daniel Matney <danieljmatney@gmail.com>
2019-10-14 01:56:46 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,12 +1,14 @@
 class Meal
   attr_reader :waiter, :customer, :total, :tip
-
+  @@all = []
 
   def initialize(waiter, customer, total, tip)
 
   end
   
-  
+  def self.all
+    @@all
+  end
   
   
   
